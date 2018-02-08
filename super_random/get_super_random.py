9345766179dd0aa2df4a7632b5ce810eaad025de
790209714@qq.com <790209714@qq.com>
2018-02-08 16:47:06 +0800

    Fixed typos

--- super_random/get_super_random.py
+++ super_random/get_super_random.py
@@ -12,24 +12,27 @@ which updates rates daily around 4PM CET every working day.
 import requests
 import time
 
+__author__ = 'hudson_huang'
+EUR_USD_url = "https://api.fixer.io/latest?symbols=USD"
+target_currency_list = ["USD"]
 
 def get_super_random(currency = 'USD',bit = 2, get_time_ticks = False):
-    assert currency in ["USD"]
+    assert currency in target_currency_list
     assert bit < 5
-    if currency == "USD":
-        random, time_ticks = get_EUR_USD_random(bit)
-        if get_time_ticks == False:
-            return random
-        else:
-            return random, time_ticks
+    random, time_ticks = url_get_random(bit,currency = currency, url = EUR_USD_url)
+    if get_time_ticks == False:
+        return random
+    else:
+        return random, time_ticks
             
-def get_EUR_USD_random(bit):
-    url = "https://api.fixer.io/latest?symbols=USD"
+def url_get_random(bit, currency, url):
+    reg = ['"',currency,'":'] 
+    reg = ''.join(reg)
     price_json = requests.get(url)
     time_ticks = time.time()
     price_content = price_json.content
     content_str = "".join(map(chr, price_content))
-    content_list_forward = content_str.split('"USD":')[1]
+    content_list_forward = content_str.split(reg)[1]
     current_price = content_list_forward.split('}')[0]
     current_price = float(current_price)
     current_price_big = int(current_price*10000)
@@ -40,4 +43,4 @@ def get_EUR_USD_random(bit):
 
 if __name__ == "__main__":
     # test super random
-    print(get_super_random())
+     assert type(get_super_random(currency = 'USD')) is int
